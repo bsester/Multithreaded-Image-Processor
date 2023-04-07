5bfe7948ed51343042dbe9250177e62106780912
jmcarni <103209370+jmcarni@users.noreply.github.com>
2023-04-07 18:17:34 -0500

    Add files via upload
    Changes the type of contrast from int to double to handle values 0<x<1
    Added 3rd option to change contrast and brightness at the same time

--- BrightContrast.cpp
+++ BrightContrast.cpp
@@ -11,7 +11,8 @@ using namespace std;
 
 int main(int argc, char** argv){
     //Serial implementation of changing the brightness and contrast of an image
-    int width, height, channels, bright, contrast; //dimensions of image --- bright, contrast hold user inputs respectively
+    int width, height, channels, bright; //dimensions of image, bright holds input from user for changing brightness
+    double contrast;  // holds input from user for changning contrast
     string fileName, opt; //name of the input image, opt holds input of user for program function option
     unsigned char *orgImg; //pointer to input image
 
@@ -42,10 +43,11 @@ int main(int argc, char** argv){
     cout << "\nPlease Select One of the Options: " << endl;
     cout << "1. Change the Brightness of the Image" << endl;
     cout << "2. Change the Contrast of the Image" << endl;
+    cout << "3. Change both the Brightness and Contrast of the Image" << endl;
     cout << "Enter an option: ";
     cin >> opt;
 
-    while(opt != "1" && opt != "2")
+    while(opt != "1" && opt != "2" && opt != "3")
     {
         cout << "\nInvalid Choice. Please enter a valid option: ";
         cin >> opt;
@@ -70,7 +72,7 @@ int main(int argc, char** argv){
 
         stbi_write_jpg(modFileName, width, height, channels, modImg, 100); //write the new image
     }
-    else //opt == 2
+    else if(opt == "2")
     {
         cout << "\nPlease enter a value greater than 0: ";
         cin  >> contrast;
@@ -87,6 +89,35 @@ int main(int argc, char** argv){
             *np = fmin(*p * contrast, 255);
             *(np + 1) = fmin(*(p + 1) * contrast, 255);
             *(np + 2) = fmin(*(p + 2) * contrast, 255);
+        }
+
+        stbi_write_jpg(modFileName, width, height, channels, modImg, 100); //write the new image
+    }
+    else //opt == 3
+    {   //Get both the brightness and contrast
+        cout << "\nPlease enter a value in the interval [0, 255]: ";
+        cin >> bright;
+        while(bright < 0 || bright > 255)
+        {
+            cout <<"\nValue not in the interval.\nPlease enter a value in the interval [0.255]: ";
+            cin >> bright;
+        }
+
+        cout << "\nPlease enter a value greater than 0: ";
+        cin  >> contrast;
+
+        while(contrast < 0)
+        {
+            cout << "\nInvalid input. Please enter a value greater than 0: ";
+            cin >> contrast;
+        }
+
+        for(unsigned char *p = orgImg, *np = modImg; p != orgImg + (width * height * channels); p+= channels, np += channels)
+        {   //Loops through each pixel, and each RGB value is scaled and increased by the values provided by the user. 
+            //Goes to 255 if the new value is greater than 255
+            *np = fmin((*p * contrast) + bright, 255);
+            *(np + 1) = fmin((*(p + 1) * contrast) + bright, 255);
+            *(np + 2) = fmin((*(p + 2) * contrast) + bright, 255);
         }
 
         stbi_write_jpg(modFileName, width, height, channels, modImg, 100); //write the new image
